043b0e209a753beab764f3ea1307a2fba0d1cfad
Amir <baradaran13085@gmail.com>
2025-02-01 22:17:51 +0330

    Create index.jsx

---
+++ front-end/src/Pages/Home/Slider/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./index.css"
+export default function Slider() {
+  return (
+    <section className='slider'>
+      <img src="" alt="" className='show' />
+    </section>
+  )
+}

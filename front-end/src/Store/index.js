f2291dcefd1f573c03b7e05d10f6d5f5cddd75e3
Amir <baradaran13085@gmail.com>
2025-02-01 22:17:47 +0330

    Update index.js

--- front-end/src/Store/index.js
+++ front-end/src/Store/index.js
@@ -1,1 +1,10 @@
-import { configDotenv } from "dotenv";
+import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from './Slices/AuthSlice'
+import cartSliceReducer from './Slices/CartSlice'
+const store=configureStore({
+    reducer:{
+        auth:authSliceReducer,
+        cart:cartSliceReducer
+    }
+})
+export default store

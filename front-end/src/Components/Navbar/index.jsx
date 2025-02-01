54fa721dea402e3bafcf47692c0f3e547e516855
Amir <baradaran13085@gmail.com>
2025-02-01 18:21:57 +0330

    update

--- front-end/src/Components/Navbar/index.jsx
+++ front-end/src/Components/Navbar/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import {
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React from "react";
 export default function Navbar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Stack flexDirection={"row"} gap={"20px"} alignItems={"center"}>
+      <Typography sx={{ color: "white", fontSize: "32px" }} component={"h1"}>
+        House Shop
+      </Typography>
+    </Stack>
+  );
 }

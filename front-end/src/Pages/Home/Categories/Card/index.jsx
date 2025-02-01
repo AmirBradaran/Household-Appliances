bc65aa7b7ac129bd9b8adf46f3e2bfde3812fad0
Amir <baradaran13085@gmail.com>
2025-02-01 22:18:00 +0330

    Create index.jsx

---
+++ front-end/src/Pages/Home/Categories/Card/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.css"
+export default function Card() {
+  return (
+    <div className='card-Ct-Home' >
+      <img src="" alt="" />
+      <h3>
+        هدست جی کلاس
+      </h3>
+      <p> تومان 8.800.000</p>
+    </div>
+  )
+}

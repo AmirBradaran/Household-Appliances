b7a7c5dc3622ce15b61f3afba1ff5c31d069cc66
Amir <baradaran13085@gmail.com>
2025-02-02 20:22:18 +0330

    summary

--- front-end/src/Pages/Home/Categories/index.jsx
+++ front-end/src/Pages/Home/Categories/index.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
-import "./index.css"
+import "./index.css";
 export default function Categories() {
   return (
-    <Grid container spacing={2} sx={{my:2}}>
-      <Grid item xs={6} md={3} height={150} sx={{border:1 , borderRadius:2 }}>
-        <Link to={"/"}> <img src="image1.jpg" alt="Category 1" /> </Link>
+    <Grid container spacing={2} sx={{ my: 2 }}>
+      <Grid item xs={6} md={3} height={150} sx={{ border: 1, borderRadius: 2 }}>
+        <Link to={"/"}>
+          <img src="image1.jpg" alt="Category 1" />
+        </Link>
       </Grid>
-      <Grid item xs={6} md={3} sx={{border:1 , borderRadius:2 }}>
-        <Link to={"/"}> <img src="image2.jpg" alt="Category 2" /> </Link>
+      <Grid item xs={6} md={3} sx={{ border: 1, borderRadius: 2 }}>
+        <Link to={"/"}>
+          <img src="image2.jpg" alt="Category 2" />
+        </Link>
       </Grid>
-      <Grid item xs={6} md={6} sx={{border:1 , borderRadius:2 }}>
-        <Link to={"/"}> <img src="image3.jpg" alt="Category 3" /> </Link>
+      <Grid item xs={6} md={6} sx={{ border: 1, borderRadius: 2 }}>
+        <Link to={"/"}>
+          <img src="image3.jpg" alt="Category 3" />
+        </Link>
       </Grid>
-      <Grid item xs={6} md={6} height={150} sx={{border:1 , borderRadius:2 }} >
-        <Link to={"/"}> <img src="image4.jpg" alt="Category 4" /> </Link>
+      <Grid item xs={6} md={6} height={150} sx={{ border: 1, borderRadius: 2 }}>
+        <Link to={"/"}>
+          <img src="image4.jpg" alt="Category 4" />
+        </Link>
       </Grid>
-      <Grid item xs={6} md={3} sx={{border:1 , borderRadius:2 }}>
-        <Link to={"/"}> <img src="image5.jpg" alt="Category 5" /> </Link>
+      <Grid item xs={6} md={3} sx={{ border: 1, borderRadius: 2 }}>
+        <Link to={"/"}>
+          <img src="image5.jpg" alt="Category 5" />
+        </Link>
       </Grid>
-      <Grid item xs={6} md={3} sx={{border:1 , borderRadius:2 }}>
-        <Link to={"/"}> <img src="image6.jpg" alt="Category 6" /> </Link>
+      <Grid item xs={6} md={3} sx={{ border: 1, borderRadius: 2 }}>
+        <Link to={"/"}>
+          <img src="image6.jpg" alt="Category 6" />
+        </Link>
       </Grid>
     </Grid>
   );

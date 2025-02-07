ef05937e4a7c2b6647084260fcd5750ebc9063d6
Amir <baradaran13085@gmail.com>
2025-02-07 14:18:39 +0330

    Update Index.jsx

--- front-end/src/Pages/Auth/Index.jsx
+++ front-end/src/Pages/Auth/Index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
+import { Box, Stack } from "@mui/material";
 
 export default function Auth() {
+  const [pageType, setPageType] = useState("login");
+  const handlePageType = () => {
+    setPageType(pageType == "login" ? "register" : "login");
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      {pageType == "login" ? (
+        <Login handlePageType={handlePageType} />
+      ) : (
+        <Register handlePageType={handlePageType} />
+      )}
+    </>
+  );
 }

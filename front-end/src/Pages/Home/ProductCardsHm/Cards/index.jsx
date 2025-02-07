7de82d3729701357982f7eae4818264f24d55b76
Amir <baradaran13085@gmail.com>
2025-02-07 14:18:47 +0330

    Update index.jsx

--- front-end/src/Pages/Home/ProductCardsHm/Cards/index.jsx
+++ front-end/src/Pages/Home/ProductCardsHm/Cards/index.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
-import "./index.css"
-import { Stack, Typography } from '@mui/material'
+import React, { useState } from "react";
+import "./index.css";
+import { Stack, Typography } from "@mui/material";
 export default function Cards() {
-  const[imgProduct,setImgProduct]=useState()
-  const[titleProduct,setTitleProduct]=useState()
-  const[priceProductCart,setPriceProductCart]=useState()
+  const [imgProduct, setImgProduct] = useState();
+  const [titleProduct, setTitleProduct] = useState();
+  const [priceProductCart, setPriceProductCart] = useState();
   return (
-    <Stack sx={{border:"1px solid black" , height:"300px", width:"225px" , mx:10 , my:5 , borderRadius:2.5}}>
+    <Stack
+      sx={{
+        border: "1px solid white",
+        height: "300px",
+        width: "225px",
+        borderRadius: 2.5,
+        background: "#dfe4e8",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        boxShadow: "3px 3px 8px gray",
+      }}
+    >
       <img src={imgProduct} />
-      <Typography level:h4 >{titleProduct}</Typography>
+      <Typography level:h4>{titleProduct}</Typography>
       <Typography>{priceProductCart}</Typography>
     </Stack>
-  )
+  );
 }

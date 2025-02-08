d87961b616ae920df23290989cf1be56cc9204ad
Amir <baradaran13085@gmail.com>
2025-02-09 00:54:30 +0330

    Update index.jsx

--- front-end/src/Pages/Home/index.jsx
+++ front-end/src/Pages/Home/index.jsx
@@ -1,9 +1,20 @@
+import SliderHm from "./SliderHm/index"
+import CategoriesHm from "./CategoriesHm/index"
+import ProductCardsHm from "./ProductCardsHm/index"
+import Suggestions from "./Suggestions/index"
 import React from 'react'
+import { Stack } from "@mui/material"
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <Stack sx={{py:5}}>
+      <Stack sx={{display:"flex", flexDirection:"row" , flexWrap:"wrap" , justifyContent:"center" , alignItems:"center" , mx:12.5 }}>
+      <Suggestions/>
+      <SliderHm/>
+      <Suggestions/>
+      </Stack>
+      <CategoriesHm/>
+      <ProductCardsHm/>
+    </Stack>
   )
 }

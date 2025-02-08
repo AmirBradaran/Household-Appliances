6cd3e6e791424436d81f3f935ab48d832efbed62
Amir <baradaran13085@gmail.com>
2025-02-09 00:54:26 +0330

    Update index.jsx

--- front-end/src/Pages/Home/SliderHm/index.jsx
+++ front-end/src/Pages/Home/SliderHm/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import './index.css';
 
 export default function SliderHm() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+        spaceBetween={20}
+        slidesPerView={'auto'}
+        style={{
+          width: '55%',
+          height: '400px',
+          border: '1px solid black',
+          borderRadius: 10,
+        }}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }

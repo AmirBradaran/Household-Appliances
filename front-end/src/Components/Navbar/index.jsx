35eaff4129ffbaed7b7376f56b8bb112534833c9
Amir <baradaran13085@gmail.com>
2025-02-09 00:54:33 +0330

    Update index.jsx

--- front-end/src/Components/Navbar/index.jsx
+++ front-end/src/Components/Navbar/index.jsx
@@ -1,5 +1,5 @@
-import { Badge, Box, IconButton, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Badge, Box, Button, IconButton, Menu, MenuItem, Stack, Typography } from "@mui/material";
 import "../../index.css";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
@@ -7,11 +7,23 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShuffleIcon from "@mui/icons-material/Shuffle";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+
 export default function Navbar() {
   const { token } = useSelector((state) => state.auth);
   const cartLength = useSelector((state) => state.cart.items).length;
   const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Stack
       sx={{
@@ -26,35 +38,47 @@ export default function Navbar() {
         boxShadow: "1px 1px 5px 5px gray",
       }}
     >
-      <Stack sx={{display:"flex" , flexDirection:"row" , alignItems:"center" , gap:0.5 , position:"absolute" , left:10 , top:"10%"}}>
+      <Stack
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 0.5,
+          position: "absolute",
+          left: 10,
+          top: "10%",
+        }}
+      >
         <Link to={"/cart"}>
           <IconButton aria-label="cart">
-            <Badge badgeContent={cartLength} color="primary" >
+            <Badge badgeContent={cartLength} color="primary">
               <CardGiftcardIcon
                 sx={{
-                  fontSize:"35px",
+                  fontSize: "35px",
                   color: "white",
-                  background:"linear-gradient(to right, var(--seven-color), var(--fifth-color))",
-                  borderRadius:"100%",
-                  padding:1.25,
-                  height:"60px",
-                  width:"60px"
+                  background:
+                    "linear-gradient(to right, var(--seven-color), var(--fifth-color))",
+                  borderRadius: "100%",
+                  padding: 1.25,
+                  height: "60px",
+                  width: "60px",
                 }}
               />
             </Badge>
           </IconButton>
         </Link>
         <Link to={""}>
-        <IconButton aria-label="like">
-          <FavoriteBorderIcon sx={{ color: "white", fontSize: "35px"}} />
-        </IconButton>
+          <IconButton aria-label="like">
+            <FavoriteBorderIcon sx={{ color: "white", fontSize: "35px" }} />
+          </IconButton>
         </Link>
         <Link>
-        <IconButton aria-label="search">
-          <SearchIcon sx={{ color: "white", fontSize: "35px" }} />
-        </IconButton>
+          <IconButton aria-label="search">
+            <SearchIcon sx={{ color: "white", fontSize: "35px" }} />
+          </IconButton>
         </Link>
-        </Stack>
+      </Stack>
+
       <Link to={"/"}>
         <DarkModeIcon
           sx={{
@@ -66,11 +90,38 @@ export default function Navbar() {
           }}
         />
       </Link>
-      <Stack sx={{position:"absolute" , right:10 , top:"25%" , direction:"rtl" , display:"flex" , alignItems:"center"}}>
-        <Link to={"/about-us"} style={{textDecoration:"none" , color:"var(--first-color)"}}>
-        <Typography sx={{fontSize:"20px" , fontWeight:"bolder"}}>
-          درباره ما
-        </Typography>
+
+      <Stack
+        sx={{
+          position: "absolute",
+          right: 10,
+          top: "25%",
+          direction: "rtl",
+          display: "flex",
+          flexDirection: "row",
+          gap: 2.5,
+          alignItems: "center",
+        }}
+      >
+        <Button onClick={handleMenuClick} style={{ textDecoration: "none", color: "var(--first-color)" , fontSize:"20px" , fontWeight:"bolder"}}>
+          دسته بندی ها
+        </Button>
+        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+          <MenuItem onClick={handleClose} style={{background:"black" , color:"white"}}>لوازم جانبی</MenuItem>
+          <MenuItem onClick={handleClose}>هدفون</MenuItem>
+          <MenuItem onClick={handleClose}>هدست</MenuItem>
+          <MenuItem onClick={handleClose}>هندزفری</MenuItem>
+        </Menu>
+
+        <Link to={"/"} style={{ textDecoration: "none", color: "var(--first-color)" }}>
+          <Typography sx={{ fontSize: "20px", fontWeight: "bolder" }}>
+            مجله ها
+          </Typography>
+        </Link>
+        <Link to={"/about-us"} style={{ textDecoration: "none", color: "var(--first-color)" }}>
+          <Typography sx={{ fontSize: "20px", fontWeight: "bolder" }}>
+            درباره ما
+          </Typography>
         </Link>
       </Stack>
     </Stack>

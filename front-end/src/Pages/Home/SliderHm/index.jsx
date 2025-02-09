78dc75bb70950e05d39a87990c4f5a7a81943212
Amir <baradaran13085@gmail.com>
2025-02-09 21:26:54 +0330

    Update index.jsx

--- front-end/src/Pages/Home/SliderHm/index.jsx
+++ front-end/src/Pages/Home/SliderHm/index.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Autoplay } from 'swiper/modules';
 import './index.css';
 
 export default function SliderHm() {
   return (
     <>
       <Swiper
-        pagination={{
-          dynamicBullets: true,
-        }}
-        modules={[Pagination]}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        pagination={{ dynamicBullets: true }}
+        loop={true}
+        modules={[Autoplay, Pagination]}
         className="mySwiper"
         spaceBetween={20}
-        slidesPerView={'auto'}
+        slidesPerView={1}
         style={{
-          width: '55%',
-          height: '400px',
+          width: '50%',
+          height: '40vh',
           border: '1px solid black',
           borderRadius: 10,
+          border:"none"
         }}
       >
         <SwiperSlide>Slide 1</SwiperSlide>

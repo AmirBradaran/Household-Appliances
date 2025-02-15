f1a12ac30464e7d63266c490990399bd722a2a59
Amir <baradaran13085@gmail.com>
2025-02-16 01:22:15 +0330

    Update index.jsx

--- front-end/src/Pages/Home/index.jsx
+++ front-end/src/Pages/Home/index.jsx
@@ -2,32 +2,47 @@ import SliderHm from "./SliderHm/index";
 import CategoriesHm from "./CategoriesHm/index";
 import BestSells from "./BestSells/index";
 import NewstSells from "./NewstSells/index";
-import SuggestionSlider from "./SuggestionSlider/index";
+import SuggestionLeftSlider from "./SuggestionLeftSlider/index";
+import SuggestionRightSlider from "./SuggestionRightSlider/index";
 import React from "react";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import SuggestionCarts from "./SuggestionCarts";
 
 export default function Home() {
   return (
     <Stack sx={{ py: 5 }}>
-      <Stack
+      <Box
         sx={{
-          height:"50vh",
-          display: "flex",
-          flexDirection: "row",
-          flexWrap:"wrap",
-          px:12,
-          py:5,
-          background:"linear-gradient(to bottom, var(--forth-color), var(--white-color))"
+          display: "grid",
+          gridTemplateColumns: {
+            xs: "1fr",
+            sm: "1fr",
+            md: "1fr",
+            lg: "1fr 1fr 1fr",
+          },
+          gap: 3,
+          justifyContent: "center",
+          py: 5,
+          px: "5%",
+          borderTopRightRadius: "15%",
+          borderTopLeftRadius: "15%",
+          background: "linear-gradient(to bottom, var(--second-color), white)",
+          alignItems: "center",
         }}
       >
-        <SuggestionSlider />
-        <SliderHm />
-        <SuggestionSlider />
-      </Stack>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <SuggestionLeftSlider />
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <SliderHm />
+        </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <SuggestionRightSlider />
+        </Box>
+      </Box>
       <CategoriesHm />
-      <SuggestionCarts/>/
-      <NewstSells/>
+      <SuggestionCarts />
+      <NewstSells />
       <BestSells />
     </Stack>
   );

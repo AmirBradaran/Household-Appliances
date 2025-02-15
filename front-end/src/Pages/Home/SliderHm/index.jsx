30e42e4ac387b5105f6208971dedc3784bf51c47
Amir <baradaran13085@gmail.com>
2025-02-16 01:22:19 +0330

    Update index.jsx

--- front-end/src/Pages/Home/SliderHm/index.jsx
+++ front-end/src/Pages/Home/SliderHm/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination, Autoplay } from 'swiper/modules';
-import './index.css';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
+import "./index.css";
 
 export default function SliderHm() {
   return (
@@ -17,22 +17,77 @@ export default function SliderHm() {
         spaceBetween={20}
         slidesPerView={1}
         style={{
-          width: '50%',
-          height: '40vh',
-          border: '1px solid black',
+          width: "900px",
+          height: "40vh",
+          border: "1px solid black",
           borderRadius: 10,
-          border:"none"
+          border: "none",
+          boxShadow: "1px 1px 10px 2px black",
         }}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./C.jpg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./D.jpg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./G.jpg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./GG.jpg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./Jd.jpg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./PP.jpg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./Sn.jpg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./Sn.jpeg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="./Sp.jpg"
+            alt=""
+            style={{ backgroundRepeat: "no-repeat", backgroundSize: "contain" }}
+          />
+        </SwiperSlide>
       </Swiper>
     </>
   );
